edfdaa6147b03d4f7a3c6e18abe8b4104f016233
Karl <karl.lundfall@capgemini.com>
2018-08-24 22:48:16 +0200

    add more efficient parsing of transformation lists

--- src/transform/parse.js
+++ src/transform/parse.js
@@ -5,14 +5,34 @@ var cssNode,
     cssView,
     svgNode;
 
-export function parseCss(value) {
-  if (value === "none") return identity;
+
+
+function parseCssWithDOM(value) {
   if (!cssNode) cssNode = document.createElement("DIV"), cssRoot = document.documentElement, cssView = document.defaultView;
   cssNode.style.transform = value;
   value = cssView.getComputedStyle(cssRoot.appendChild(cssNode), null).getPropertyValue("transform");
   cssRoot.removeChild(cssNode);
-  value = value.slice(7, -1).split(",");
-  return decompose(+value[0], +value[1], +value[2], +value[3], +value[4], +value[5]);
+  return value.slice(7, -1).split(",");
+}
+
+function parseCssWithoutDOM(string) {
+  if(window.DOMMatrix){
+    var matrix = new DOMMatrix(string);
+    return [matrix.a, matrix.b, matrix.c, matrix.d, matrix.e, matrix.f];
+  }
+  return [];
+}
+export function parseCss(value) {
+  if (value === "none") return identity;
+  var parsedValue;
+  try {
+    parsedValue = parseCssWithoutDOM(value);
+  } catch(error) {
+    
+  }
+  if(!parsedValue || !parsedValue.length)
+    parsedValue = parseCssWithDOM(value);
+  return decompose(parsedValue[0], parsedValue[1], parsedValue[2], parsedValue[3], parsedValue[4], parsedValue[5]);
 }
 
 export function parseSvg(value) {
